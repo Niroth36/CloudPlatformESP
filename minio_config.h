308bfba046c9a30f3df06bc4e17deb51e9257bd5
Niroth36 <charitos_@hotmail.com>
2025-01-21 23:58:17 +0200

    minio pvc complete

--- minio_config.h
+++ minio_config.h
@@ -1,7 +1,7 @@
 // minio_config.h
 
 // MinIO Configuration
-#define MINIO_SERVER "10.1.238.97:9000"  // Replace with your MinIO server's address
+#define MINIO_SERVER "10.1.238.69:9000"  // Replace with your MinIO server's address
 #define MINIO_BUCKET "esp-data"     // Replace with your MinIO bucket name
 #define MINIO_ACCESS_KEY "inbdbQFyjOFJawQS6mkc" // Replace with your MinIO access key
 #define MINIO_SECRET_KEY "F6lbKKwdKLj3z4BC6EFQOQAAlpryPnismzURgfm2" // Replace with your MinIO secret key
